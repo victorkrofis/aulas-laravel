1afde4bb31e28887f0a37e6ac5842f3d2cc0773f
VICTOR DA SILVA OLIVEIRA <victor.soliveira21@senacsp.edu.br>
2025-06-11 09:49:28 -0300

    aula

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,8 +23,14 @@
     <link rel="mask-icon" href="{{ asset("icons/safari-pinned-tab.svg")}}" color="#712cf9">
     <link rel="icon" href="{{ asset("icons/favicon.ico") }}">
     <meta name="theme-color" content="#712cf9">
+
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.min.css" rel="stylesheet">
-    <link href="dashboard.css" rel="stylesheet">
+
+
+    <link href="{{ asset("css/dashboard.css") }}" rel="stylesheet">
+
+
     <style>
         .bd-placeholder-img {
             font-size: 1.125rem;
@@ -132,39 +138,9 @@
             </path>
         </symbol>
     </svg>
-    <div class="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle"> <button
-            class="btn btn-bd-primary py-2 dropdown-toggle d-flex align-items-center" id="bd-theme" type="button"
-            aria-expanded="false" data-bs-toggle="dropdown" aria-label="Toggle theme (auto)"> <svg
-                class="bi my-1 theme-icon-active" aria-hidden="true">
-                <use href="#circle-half"></use>
-            </svg> <span class="visually-hidden" id="bd-theme-text">Toggle theme</span> </button>
-        <ul class="dropdown-menu dropdown-menu-end shadow" aria-labelledby="bd-theme-text">
-            <li> <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light"
-                    aria-pressed="false"> <svg class="bi me-2 opacity-50" aria-hidden="true">
-                        <use href="#sun-fill"></use>
-                    </svg>
-                    Light
-                    <svg class="bi ms-auto d-none" aria-hidden="true">
-                        <use href="#check2"></use>
-                    </svg> </button> </li>
-            <li> <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark"
-                    aria-pressed="false"> <svg class="bi me-2 opacity-50" aria-hidden="true">
-                        <use href="#moon-stars-fill"></use>
-                    </svg>
-                    Dark
-                    <svg class="bi ms-auto d-none" aria-hidden="true">
-                        <use href="#check2"></use>
-                    </svg> </button> </li>
-            <li> <button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="auto"
-                    aria-pressed="true"> <svg class="bi me-2 opacity-50" aria-hidden="true">
-                        <use href="#circle-half"></use>
-                    </svg>
-                    Auto
-                    <svg class="bi ms-auto d-none" aria-hidden="true">
-                        <use href="#check2"></use>
-                    </svg> </button> </li>
-        </ul>
-    </div> <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
+    
+    
+    <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
         <symbol id="calendar3" viewBox="0 0 16 16">
             <path
                 d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857V3.857z">
@@ -245,7 +221,7 @@
         </symbol>
     </svg>
     <header class="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark"> <a
-            class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">Company name</a>
+            class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">Aurora-main</a>
         <ul class="navbar-nav flex-row d-md-none">
             <li class="nav-item text-nowrap"> <button class="nav-link px-3 text-white" type="button"
                     data-bs-toggle="collapse" data-bs-target="#navbarSearch" aria-controls="navbarSearch"
@@ -264,257 +240,25 @@
     </header>
     <div class="container-fluid">
         <div class="row">
-            <div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
-                <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu"
-                    aria-labelledby="sidebarMenuLabel">
-                    <div class="offcanvas-header">
-                        <h5 class="offcanvas-title" id="sidebarMenuLabel">Company name</h5> <button type="button"
-                            class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu"
-                            aria-label="Close"></button>
-                    </div>
-                    <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
-                        <ul class="nav flex-column">
-                            <li class="nav-item"> <a class="nav-link d-flex align-items-center gap-2 active"
-                                    aria-current="page" href="#"> <svg class="bi" aria-hidden="true">
-                                        <use xlink:href="#house-fill"></use>
-                                    </svg>
-                                    Dashboard
-                                </a> </li>
-                            <li class="nav-item"> <a class="nav-link d-flex align-items-center gap-2" href="#"> <svg
-                                        class="bi" aria-hidden="true">
-                                        <use xlink:href="#file-earmark"></use>
-                                    </svg>
-                                    Orders
-                                </a> </li>
-                            <li class="nav-item"> <a class="nav-link d-flex align-items-center gap-2" href="#"> <svg
-                                        class="bi" aria-hidden="true">
-                                        <use xlink:href="#cart"></use>
-                                    </svg>
-                                    Products
-                                </a> </li>
-                            <li class="nav-item"> <a class="nav-link d-flex align-items-center gap-2" href="#"> <svg
-                                        class="bi" aria-hidden="true">
-                                        <use xlink:href="#people"></use>
-                                    </svg>
-                                    Customers
-                                </a> </li>
-                            <li class="nav-item"> <a class="nav-link d-flex align-items-center gap-2" href="#"> <svg
-                                        class="bi" aria-hidden="true">
-                                        <use xlink:href="#graph-up"></use>
-                                    </svg>
-                                    Reports
-                                </a> </li>
-                            <li class="nav-item"> <a class="nav-link d-flex align-items-center gap-2" href="#"> <svg
-                                        class="bi" aria-hidden="true">
-                                        <use xlink:href="#puzzle"></use>
-                                    </svg>
-                                    Integrations
-                                </a> </li>
-                        </ul>
-                        <h6
-                            class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase">
-                            <span>Saved reports</span> <a class="link-secondary" href="#" aria-label="Add a new report">
-                                <svg class="bi" aria-hidden="true">
-                                    <use xlink:href="#plus-circle"></use>
-                                </svg> </a> </h6>
-                        <ul class="nav flex-column mb-auto">
-                            <li class="nav-item"> <a class="nav-link d-flex align-items-center gap-2" href="#"> <svg
-                                        class="bi" aria-hidden="true">
-                                        <use xlink:href="#file-earmark-text"></use>
-                                    </svg>
-                                    Current month
-                                </a> </li>
-                            <li class="nav-item"> <a class="nav-link d-flex align-items-center gap-2" href="#"> <svg
-                                        class="bi" aria-hidden="true">
-                                        <use xlink:href="#file-earmark-text"></use>
-                                    </svg>
-                                    Last quarter
-                                </a> </li>
-                            <li class="nav-item"> <a class="nav-link d-flex align-items-center gap-2" href="#"> <svg
-                                        class="bi" aria-hidden="true">
-                                        <use xlink:href="#file-earmark-text"></use>
-                                    </svg>
-                                    Social engagement
-                                </a> </li>
-                            <li class="nav-item"> <a class="nav-link d-flex align-items-center gap-2" href="#"> <svg
-                                        class="bi" aria-hidden="true">
-                                        <use xlink:href="#file-earmark-text"></use>
-                                    </svg>
-                                    Year-end sale
-                                </a> </li>
-                        </ul>
-                        <hr class="my-3">
-                        <ul class="nav flex-column mb-auto">
-                            <li class="nav-item"> <a class="nav-link d-flex align-items-center gap-2" href="#"> <svg
-                                        class="bi" aria-hidden="true">
-                                        <use xlink:href="#gear-wide-connected"></use>
-                                    </svg>
-                                    Settings
-                                </a> </li>
-                            <li class="nav-item"> <a class="nav-link d-flex align-items-center gap-2" href="#"> <svg
-                                        class="bi" aria-hidden="true">
-                                        <use xlink:href="#door-closed"></use>
-                                    </svg>
-                                    Sign out
-                                </a> </li>
-                        </ul>
-                    </div>
+           @include("components.navegacao")
+
+
                 </div>
             </div>
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                <div
-                    class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                    <h1 class="h2">Dashboard</h1>
-                    <div class="btn-toolbar mb-2 mb-md-0">
-                        <div class="btn-group me-2"> <button type="button"
-                                class="btn btn-sm btn-outline-secondary">Share</button> <button type="button"
-                                class="btn btn-sm btn-outline-secondary">Export</button> </div> <button type="button"
-                            class="btn btn-sm btn-outline-secondary dropdown-toggle d-flex align-items-center gap-1">
-                            <svg class="bi" aria-hidden="true">
-                                <use xlink:href="#calendar3"></use>
-                            </svg>
-                            This week
-                        </button>
-                    </div>
-                </div> <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
-                <h2>Section title</h2>
-                <div class="table-responsive small">
-                    <table class="table table-striped table-sm">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Header</th>
-                                <th scope="col">Header</th>
-                                <th scope="col">Header</th>
-                                <th scope="col">Header</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1,001</td>
-                                <td>random</td>
-                                <td>data</td>
-                                <td>placeholder</td>
-                                <td>text</td>
-                            </tr>
-                            <tr>
-                                <td>1,002</td>
-                                <td>placeholder</td>
-                                <td>irrelevant</td>
-                                <td>visual</td>
-                                <td>layout</td>
-                            </tr>
-                            <tr>
-                                <td>1,003</td>
-                                <td>data</td>
-                                <td>rich</td>
-                                <td>dashboard</td>
-                                <td>tabular</td>
-                            </tr>
-                            <tr>
-                                <td>1,003</td>
-                                <td>information</td>
-                                <td>placeholder</td>
-                                <td>illustrative</td>
-                                <td>data</td>
-                            </tr>
-                            <tr>
-                                <td>1,004</td>
-                                <td>text</td>
-                                <td>random</td>
-                                <td>layout</td>
-                                <td>dashboard</td>
-                            </tr>
-                            <tr>
-                                <td>1,005</td>
-                                <td>dashboard</td>
-                                <td>irrelevant</td>
-                                <td>text</td>
-                                <td>placeholder</td>
-                            </tr>
-                            <tr>
-                                <td>1,006</td>
-                                <td>dashboard</td>
-                                <td>illustrative</td>
-                                <td>rich</td>
-                                <td>data</td>
-                            </tr>
-                            <tr>
-                                <td>1,007</td>
-                                <td>placeholder</td>
-                                <td>tabular</td>
-                                <td>information</td>
-                                <td>irrelevant</td>
-                            </tr>
-                            <tr>
-                                <td>1,008</td>
-                                <td>random</td>
-                                <td>data</td>
-                                <td>placeholder</td>
-                                <td>text</td>
-                            </tr>
-                            <tr>
-                                <td>1,009</td>
-                                <td>placeholder</td>
-                                <td>irrelevant</td>
-                                <td>visual</td>
-                                <td>layout</td>
-                            </tr>
-                            <tr>
-                                <td>1,010</td>
-                                <td>data</td>
-                                <td>rich</td>
-                                <td>dashboard</td>
-                                <td>tabular</td>
-                            </tr>
-                            <tr>
-                                <td>1,011</td>
-                                <td>information</td>
-                                <td>placeholder</td>
-                                <td>illustrative</td>
-                                <td>data</td>
-                            </tr>
-                            <tr>
-                                <td>1,012</td>
-                                <td>text</td>
-                                <td>placeholder</td>
-                                <td>layout</td>
-                                <td>dashboard</td>
-                            </tr>
-                            <tr>
-                                <td>1,013</td>
-                                <td>dashboard</td>
-                                <td>irrelevant</td>
-                                <td>text</td>
-                                <td>visual</td>
-                            </tr>
-                            <tr>
-                                <td>1,014</td>
-                                <td>dashboard</td>
-                                <td>illustrative</td>
-                                <td>rich</td>
-                                <td>data</td>
-                            </tr>
-                            <tr>
-                                <td>1,015</td>
-                                <td>random</td>
-                                <td>tabular</td>
-                                <td>information</td>
-                                <td>text</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+               
+                @yield("content")
+
             </main>
         </div>
     </div>
-    <script src="/docs/5.3/dist/js/bootstrap.bundle.min.js"
+   <script src="{{ asset("js/bootstrap.bundle.min.js") }}"
         integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
         class="astro-vvvwv3sm"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js"
         integrity="sha384-eI7PSr3L1XLISH8JdDII5YN/njoSsxfbrkCTnJrzXt+ENP5MOVBxD+l6sEG4zoLp" crossorigin="anonymous"
         class="astro-vvvwv3sm"></script>
-    <script src="dashboard.js" class="astro-vvvwv3sm"></script>
+    <script src="{{ asset("js/dashboard.js")}}"  class="astro-vvvwv3sm"></script>
 </body>
 
-</html>
+</html> 
